ff8fc879fa84fde7822c5804098fcd4645e5dfec
Harish Chouhan <me@harishchouhan.com>
2022-01-11 14:49:58 +0530

    Update textdomain

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * Twenty Twenty-Two: Block Patterns
+ * Shanti: Block Patterns
  *
- * @since Twenty Twenty-Two 1.0
+ * @since Shanti 1.0
  */
 
 /**
  * Registers block patterns and categories.
  *
- * @since Twenty Twenty-Two 1.0
+ * @since Shanti 1.0
  */
-function twentytwentytwo_register_block_patterns() {
+function shanti_register_block_patterns() {
 	$block_pattern_categories = array(
-		'twentytwentytwo-general' => array( 'label' => __( 'Twenty Twenty-Two General', 'twentytwentytwo' ) ),
-		'twentytwentytwo-footers' => array( 'label' => __( 'Twenty Twenty-Two Footers', 'twentytwentytwo' ) ),
-		'twentytwentytwo-headers' => array( 'label' => __( 'Twenty Twenty-Two Headers', 'twentytwentytwo' ) ),
-		'twentytwentytwo-query'   => array( 'label' => __( 'Twenty Twenty-Two Posts', 'twentytwentytwo' ) ),
-		'twentytwentytwo-pages'   => array( 'label' => __( 'Twenty Twenty-Two Pages', 'twentytwentytwo' ) ),
+		'shanti-general' => array( 'label' => __( 'Shanti General', 'shanti' ) ),
+		'shanti-footers' => array( 'label' => __( 'Shanti Footers', 'shanti' ) ),
+		'shanti-headers' => array( 'label' => __( 'Shanti Headers', 'shanti' ) ),
+		'shanti-query'   => array( 'label' => __( 'Shanti Posts', 'shanti' ) ),
+		'shanti-pages'   => array( 'label' => __( 'Shanti Pages', 'shanti' ) ),
 	);
 
 	/**
 	 * Filters the theme block pattern categories.
 	 *
-	 * @since Twenty Twenty-Two 1.0
+	 * @since Shanti 1.0
 	 *
 	 * @param array[] $block_pattern_categories {
 	 *     An associative array of block pattern categories, keyed by category name.
@@ -34,7 +34,7 @@ function twentytwentytwo_register_block_patterns() {
 	 *     }
 	 * }
 	 */
-	$block_pattern_categories = apply_filters( 'twentytwentytwo_block_pattern_categories', $block_pattern_categories );
+	$block_pattern_categories = apply_filters( 'shanti_block_pattern_categories', $block_pattern_categories );
 
 	foreach ( $block_pattern_categories as $name => $properties ) {
 		register_block_pattern_category( $name, $properties );
@@ -113,17 +113,17 @@ function twentytwentytwo_register_block_patterns() {
 	/**
 	 * Filters the theme block patterns.
 	 *
-	 * @since Twenty Twenty-Two 1.0
+	 * @since Shanti 1.0
 	 *
 	 * @param $block_patterns array List of block patterns by name.
 	 */
-	$block_patterns = apply_filters( 'twentytwentytwo_block_patterns', $block_patterns );
+	$block_patterns = apply_filters( 'shanti_block_patterns', $block_patterns );
 
 	foreach ( $block_patterns as $block_pattern ) {
 		register_block_pattern(
-			'twentytwentytwo/' . $block_pattern,
+			'shanti/' . $block_pattern,
 			require __DIR__ . '/patterns/' . $block_pattern . '.php'
 		);
 	}
 }
-add_action( 'init', 'twentytwentytwo_register_block_patterns', 9 );
+add_action( 'init', 'shanti_register_block_patterns', 9 );
--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 /**
  * Without this file, WordPress will consider the theme broken.
  *
- * @package hcblocks
+ * @package shanti
  */
 
 if ( current_user_can( 'activate_plugins' ) ) {
-	esc_html_e( 'This theme was built for the WordPress experimental full site editing feature. You need to install and activate the Gutenberg plugin to make it work. ', 'hcblocks' );
+	esc_html_e( 'This theme was built for the WordPress experimental full site editing feature. You need to install and activate the Gutenberg plugin to make it work. ', 'shanti' );
 }
