24ddbc9d1e6133a42d0ada12e18e14b44cc31454
Harish Chouhan <me@harishchouhan.com>
2022-01-15 07:28:13 +0530

    Serve google fonts locally

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
-
 if ( ! function_exists( 'shanti_setup' ) ) :
 
 	/**
@@ -29,41 +28,33 @@ if ( ! function_exists( 'shanti_setup' ) ) :
 	}
 
 endif;
-
 add_action( 'after_setup_theme', 'shanti_setup' );
 
-
-// Enqueue style sheet.
-add_action( 'wp_enqueue_scripts', 'shanti_enqueue_style_sheet' );
-function shanti_enqueue_style_sheet() {
-
-	wp_enqueue_style( 'shanti', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
-
+/**
+ * Enqueue scripts and styles.
+ */
+add_action( 'wp_enqueue_scripts', 'shanti_scripts' );
+function shanti_scripts() {
+	// Enqueue fonts stylesheet.
+	wp_enqueue_style( 'shanti-fonts', shanti_fonts_url(), array(), wp_get_theme()->get( 'Version' ) );
+	// Theme stylesheet.
+	wp_enqueue_style( 'shanti-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 
-// Enqueue fonts.
-add_action( 'wp_enqueue_scripts', 'shanti_enqueue_fonts' );
-function shanti_enqueue_fonts() {
-
-	wp_enqueue_style( 'shanti-fonts', shanti_fonts_url(), array(), null );
-
-}
-
-
-// Define fonts.
+/**
+ * Get Google fonts and save locally with WPTT Webfont Loader.
+ */
 function shanti_fonts_url() {
+	$font_families = array(
+		'Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600'
+	);
 
-	// Allow child themes to disable to the default Frost fonts.
-	$dequeue_fonts = apply_filters( 'shanti_fonts_url', false );
+	$fonts_url = add_query_arg( array(
+		'family' => implode( '&family=', $font_families ),
+		'display' => 'swap',
+	), 'https://fonts.googleapis.com/css2' );
 
-	if ( $dequeue_fonts ) {
-		return '';
-	}
+	require_once get_theme_file_path( 'inc/wptt-webfont-loader.php' );
 
-	$fonts = [
-		'family=Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600'
-	];
-
-	// Make a single request for all Google Fonts.
-	return esc_url_raw( 'https://fonts.googleapis.com/css2?' . implode( '&', array_unique( $fonts ) ) . '&display=swap' );
+	return wptt_get_webfont_url( esc_url_raw( $fonts_url ) );
 }
