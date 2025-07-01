fe5a35a4204e39020e64bc36001bb21e260d7f47
Harish Chouhan <me@harishchouhan.com>
2025-07-01 20:49:50 +0700

    Some Update

--- inc/patterns/general/general-heading.php
+++ inc/patterns/general/general-heading.php
@@ -9,14 +9,14 @@ return array(
 	'title'      => __( 'Header with divider.', 'shanti' ),
 	'categories' => array( 'shanti-general' ),
 	'blockTypes' => array( 'core/template-part/general' ),
-	'content'    => '<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
-					<div class="wp-block-columns alignwide are-vertically-aligned-center">
+	'content'    => '<!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide"} -->
+					<div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile">
 
 						<!-- wp:column {"verticalAlignment":"center"} -->
 						<div class="wp-block-column is-vertically-aligned-center">
 
 							<!-- wp:heading {"fontSize":"x-large"} -->
-							<h2 class="has-x-large-font-size">Latest posts<br>about travel</h2>
+							<h2 class="has-x-large-font-size">Latest posts</h2>
 							<!-- /wp:heading -->
 
 						</div>
@@ -25,16 +25,17 @@ return array(
 						<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0rem","bottom":"0rem","right":"0rem","left":"0rem"}}}} -->
 						<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem">
 
-							<!-- wp:separator {"color":"primary","className":"is-style-wide"} -->
-							<hr class="wp-block-separator has-text-color has-background has-primary-background-color has-primary-color is-style-wide"/>
+							<!-- wp:separator {"opacity":"css","backgroundColor":"primary","className":"is-style-wide"} -->
+							<hr class="wp-block-separator has-text-color has-primary-color has-css-opacity has-primary-background-color has-background is-style-wide"/>
 							<!-- /wp:separator -->
-
-							<!-- wp:paragraph {"align":"right"} -->
-							<p class="has-text-align-right">List of posts</p>
-							<!-- /wp:paragraph -->
 
 						</div>
 						<!-- /wp:column -->
+
 					</div>
-					<!-- /wp:columns -->',
+					<!-- /wp:columns -->
+
+					<!-- wp:spacer {"height":"40px"} -->
+					<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+					<!-- /wp:spacer -->',
 );
