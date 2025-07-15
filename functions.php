aab3ad2e4184d8d26a44d05ff008f74d32a6d449
Harish Chouhan <me@harishchouhan.com>
2025-07-15 20:45:56 +0700

    Move block styles to functions.php and remove block-styles.php for simplicity

--- functions.php
+++ functions.php
@@ -114,10 +114,39 @@ if ( ! function_exists( 'shanti_restore_customizer' ) ) {
 add_action( 'customize_register', 'shanti_restore_customizer' );
 
 
-/**
- * Include theme-specific block styles.
- */
-require get_template_directory() . '/inc/block-styles.php';
+if ( ! function_exists( 'shanti_register_block_styles' ) ) {
+	/**
+	 * Register block styles.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
+	function shanti_register_block_styles() {
+
+		$block_styles = array(
+			'core/group' => array(
+				'full-height' => __( 'Full height', 'shanti' ),
+			),
+			'core/cover' => array(
+				'full-height' => __( 'Full height', 'shanti' ),
+			),
+		);
+
+		foreach ( $block_styles as $block => $styles ) {
+			foreach ( $styles as $style_name => $style_label ) {
+				register_block_style(
+					$block,
+					array(
+						'name'  => $style_name,
+						'label' => $style_label,
+					)
+				);
+			}
+		}
+	}
+}
+add_action( 'init', 'shanti_register_block_styles' );
 
 /**
  * Include theme-specific block patterns.
