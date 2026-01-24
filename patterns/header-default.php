82191fab1739169f71ee34fd6ff7ee81766e5bf9
Harish Chouhan <me@harishchouhan.com>
2026-01-24 18:05:00 +0700

    update header-default to use outer spacing
    - change xxx-large to outer spacing preset
    - add inserter flag

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -4,10 +4,12 @@
  * Slug: shanti/header-default
  * Categories: shanti-header
  * Block Types: core/template-part/header
+ * Inserter: true
  */
 ?>
-<!-- wp:group {"align":"full","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--xxx-large)","right":"var(--wp--preset--spacing--xxx-large)","bottom":"var(--wp--preset--spacing--xxx-large)","left":"var(--wp--preset--spacing--xxx-large)"}}}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--xxx-large);padding-right:var(--wp--preset--spacing--xxx-large);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--xxx-large)">
+<!-- wp:group {"align":"full","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--outer)","right":"var(--wp--preset--spacing--outer)","bottom":"var(--wp--preset--spacing--outer)","left":"var(--wp--preset--spacing--outer)"}}}} -->
+<div class="wp-block-group alignfull"
+	style="padding-top:var(--wp--preset--spacing--outer);padding-right:var(--wp--preset--spacing--outer);padding-bottom:var(--wp--preset--spacing--outer);padding-left:var(--wp--preset--spacing--outer)">
 
 	<!-- wp:group {"align":"wide","className":"is-layout-flex","layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal"}} -->
 	<div class="wp-block-group alignwide is-layout-flex">
