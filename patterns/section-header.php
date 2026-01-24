03eb3d092fdb4755b34c45b449313fadc5accf6d
Harish Chouhan <me@harishchouhan.com>
2026-01-24 18:05:06 +0700

    use spacing preset instead of hardcoded values in section-header
    - replace hardcoded spacing values with small preset

--- patterns/section-header.php
+++ patterns/section-header.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|small"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--30)">
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--small)">
 
 	<!-- wp:heading {"level":2} -->
 	<h2 class="wp-block-heading">
@@ -20,5 +20,5 @@
 
 <!-- wp:separator {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|small"}}},"backgroundColor":"accent-light","className":"is-style-wide"} -->
 <hr class="wp-block-separator alignwide has-text-color has-accent-light-color has-alpha-channel-opacity has-accent-light-background-color has-background is-style-wide"
-	style="margin-top:0;margin-bottom:var(--wp--preset--spacing--40)" />
+	style="margin-top:0;margin-bottom:var(--wp--preset--spacing--small)" />
 <!-- /wp:separator -->
