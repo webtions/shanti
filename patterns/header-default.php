6acb4913df6ec78ede7b16b9e446a6eb7bd78d5e
Harish Chouhan <me@harishchouhan.com>
2026-02-07 12:35:57 +0700

    Clean up header-default pattern

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -5,14 +5,19 @@
  * Categories: shanti-header
  * Block Types: core/template-part/header
  * Inserter: true
+ *
+ * @package Shanti
  */
-?>
-<!-- wp:group {"align":"full","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--outer)","right":"var(--wp--preset--spacing--outer)","bottom":"var(--wp--preset--spacing--outer)","left":"var(--wp--preset--spacing--outer)"}}}} -->
-<div class="wp-block-group alignfull"
-	style="padding-top:var(--wp--preset--spacing--outer);padding-right:var(--wp--preset--spacing--outer);padding-bottom:var(--wp--preset--spacing--outer);padding-left:var(--wp--preset--spacing--outer)">
 
-	<!-- wp:group {"align":"wide","className":"is-layout-flex","layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal"}} -->
-	<div class="wp-block-group alignwide is-layout-flex">
+?>
+<!-- wp:group {"align":"full","layout":{"type":"default"},"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--outer)","bottom":"var(--wp--preset--spacing--outer)","left":"var(--wp--preset--spacing--outer)","right":"var(--wp--preset--spacing--outer)"}}}} -->
+<div class="wp-block-group alignfull"
+	style="padding-top:var(--wp--preset--spacing--outer);padding-bottom:var(--wp--preset--spacing--outer);padding-left:var(--wp--preset--spacing--outer);padding-right:var(--wp--preset--spacing--outer)">
+
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"align":"wide","className":"is-layout-flex","layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal"}} -->
+		<div class="wp-block-group alignwide is-layout-flex">
 
 		<!-- wp:group {"className":"is-layout-flex","layout":{"type":"flex"}} -->
 		<div class="wp-block-group is-layout-flex">
@@ -22,6 +27,8 @@
 
 		<!-- wp:navigation {"isResponsive":true,"layout":{"type":"flex"}} /-->
 
+		</div>
+		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->
 
