150e9492eb8e74dcdfb75e12da0459be21443090
Harish Chouhan <me@harishchouhan.com>
2026-02-14 07:08:11 +0700

    Remove bottom margin from hidden section header pattern

--- patterns/hidden-section-header.php
+++ patterns/hidden-section-header.php
@@ -11,8 +11,8 @@
 
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--small)">
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;">
 	<!-- wp:heading {"align":"wide"} -->
 	<h2 class="wp-block-heading alignwide">Latest Posts</h2>
 	<!-- /wp:heading -->
