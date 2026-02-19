64aa9d1c23913cded67955ba1658a56c29807246
Harish Chouhan <me@harishchouhan.com>
2026-02-19 08:46:06 +0700

    Use esc_html_x for share heading and make Twitter label translatable

--- patterns/post-share.php
+++ patterns/post-share.php
@@ -17,14 +17,14 @@
 <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-bottom: var(--wp--preset--spacing--70)">
 	<!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
-	<h2 class="wp-block-heading has-text-align-center has-large-font-size"><?php esc_html_e( 'Share this post', 'shanti' ); ?></h2>
+	<h2 class="wp-block-heading has-text-align-center has-large-font-size"><?php echo esc_html_x( 'Share this post', 'Share section heading', 'shanti' ); ?></h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:outermost/social-sharing {"iconColor":"foreground","iconColorValue":"#000000","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
 	<ul class="wp-block-outermost-social-sharing has-icon-color is-style-logos-only">
 		<!-- wp:outermost/social-sharing-link {"service":"facebook"} /-->
 
-		<!-- wp:outermost/social-sharing-link {"service":"twitter","label":"Share on X"} /-->
+		<!-- wp:outermost/social-sharing-link {"service":"twitter","label":"<?php echo esc_attr( _x( 'Share on X', 'Social share label', 'shanti' ) ); ?>"} /-->
 
 		<!-- wp:outermost/social-sharing-link {"service":"linkedin"} /-->
 
