aa19b64641925950aec4e5780c904e3e84feeae0
Harish Chouhan <me@harishchouhan.com>
2026-02-28 00:19:25 +0700

    Add hidden blog heading pattern for index accessibility

---
+++ patterns/hidden-blog-heading.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Hidden blog heading
+ * Description: Visually hidden heading for the index template (accessibility).
+ * Slug: shanti/hidden-blog-heading
+ * Inserter: false
+ *
+ * @package Shanti
+ */
+
+?>
+<!-- wp:heading {"textAlign":"left","level":1} -->
+<h1 class="wp-block-heading has-text-align-left"><?php esc_html_e( 'Blog', 'shanti' ); ?></h1>
+<!-- /wp:heading -->
