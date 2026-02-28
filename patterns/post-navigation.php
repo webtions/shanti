efdb8278af9da8deaff8b9d0e2ffa2dd3668ffab
Harish Chouhan <me@harishchouhan.com>
2026-02-28 17:02:08 +0700

    Rename background color preset to base and update all references

--- patterns/post-navigation.php
+++ patterns/post-navigation.php
@@ -16,7 +16,7 @@
 ?>
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"color":"var:preset|color|background","width":"2px"},"left":{"width":"0px","style":"none"}}}} -->
-<div class="wp-block-group alignwide" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--background);border-bottom-width:2px;border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--80)">
+<div class="wp-block-group alignwide" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--base);border-bottom-width:2px;border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--80)">
 	<!-- wp:columns {"className":"alignwide"} -->
 	<div class="wp-block-columns alignwide">
 		<!-- wp:column {"width":"50%"} -->
