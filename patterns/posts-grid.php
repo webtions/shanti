8d29c62f9bef1bf0f30695ec8da71031877ebe51
Harish Chouhan <me@harishchouhan.com>
2026-03-06 06:50:42 +0700

    Use blog post card and hidden patterns in posts grid

--- patterns/posts-grid.php
+++ patterns/posts-grid.php
@@ -6,6 +6,7 @@
  * Categories: posts
  * Keywords: query, loop, grid, posts, blog
  * Block Types: core/query
+ * Post Types: page, wp_template
  * Viewport Width: 1600
  * Inserter: true
  *
@@ -18,51 +19,12 @@
 <div class="wp-block-query alignwide">
 
 	<!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"grid","columnCount":3}} -->
-
-	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-
-		<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/5","width":"100%"} /-->
-
-		<!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"fontSize":"x-large"} /-->
-
-		<!-- wp:post-excerpt {"excerptLength":34,"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|50"}}}} /-->
-
-		<!-- wp:pattern {"slug":"shanti/hidden-post-meta"} /-->
-
-	</div>
-	<!-- /wp:group -->
-
+	<!-- wp:pattern {"slug":"shanti/blog-post-card"} /-->
 	<!-- /wp:post-template -->
 
-	<!-- wp:query-no-results -->
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-		<!-- wp:heading {"fontSize":"large"} -->
-		<h2 class="wp-block-heading has-large-font-size"><?php echo esc_html_x( 'Nothing Found', 'Empty query message', 'shanti' ); ?></h2>
-		<!-- /wp:heading -->
+	<!-- wp:pattern {"slug":"shanti/hidden-query-no-results"} /-->
 
-		<!-- wp:paragraph -->
-		<p><?php echo esc_html_x( 'Sorry, no posts matched your criteria. Try searching for something else.', 'Empty query body', 'shanti' ); ?></p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-	<!-- /wp:query-no-results -->
-
-	<!-- wp:group {"metadata":{"name":"Query Pagination"},"className":"query-pagination","align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}}} -->
-	<div class="wp-block-group query-pagination alignwide" style="margin-top:var(--wp--preset--spacing--80)">
-
-		<!-- wp:separator {"className":"is-style-wavy","style":{"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}}} -->
-		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wavy" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80)"/>
-		<!-- /wp:separator -->
-
-		<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal"}} -->
-			<!-- wp:query-pagination-previous {"fontSize":"medium"} /-->
-			<!-- wp:query-pagination-next {"fontSize":"medium"} /-->
-		<!-- /wp:query-pagination -->
-
-	</div>
-	<!-- /wp:group -->
+	<!-- wp:pattern {"slug":"shanti/hidden-query-pagination"} /-->
 
 </div>
 <!-- /wp:query -->
